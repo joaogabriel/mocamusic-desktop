5c50c369e9122ddc676f98f8308d8e4957ec28c0
João Gabriel Esteves <jgs.esteves@gmail.com>
2024-06-04 19:52:39 -0300

    Implements download audio in Rust

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,43 +1,82 @@
-import Image from "next/image"
+"use client"
+
 import Link from "next/link"
 
-import { Button } from "@/components/ui/button"
-import { Input } from "@/components/ui/input"
-import { Label } from "@/components/ui/label"
+import {Button} from "@/components/ui/button"
+import {Input} from "@/components/ui/input"
+import {Label} from "@/components/ui/label"
+import {useForm} from "react-hook-form";
+import {z} from "zod";
+import {zodResolver} from "@hookform/resolvers/zod";
+import {useState} from "react";
+import GetVideoInfo from "@/app/usecase/GetVideoInfo";
+
+const downloadVideoSchema = z.object({
+    url: z.string().url()
+});
+
+type DownloadVideoSchema = z.infer<typeof downloadVideoSchema>;
 
 export default function Page() {
-  return (
-      <div className="w-full">
-        <div className="flex items-center justify-center py-12">
-          <div className="mx-auto grid w-[350px] gap-6">
-            <div className="grid gap-2 text-center">
-              <h1 className="text-3xl font-bold">Download</h1>
-              <p className="text-balance text-muted-foreground">
-                Insira o link do vídeo para download
-              </p>
+
+    const [downloadAvailable, setDownloadAvailable] = useState(false);
+
+    const {register, handleSubmit} = useForm<DownloadVideoSchema>({
+        resolver: zodResolver(downloadVideoSchema),
+    });
+
+    async function onSubmit(data: DownloadVideoSchema) {
+        console.log('onSubmit')
+        console.log(data)
+        setDownloadAvailable(true);
+        const getVideoInfo = new GetVideoInfo();
+        const response = await getVideoInfo.getVideoInfo(data.url);
+        const response2 = await getVideoInfo.download(data.url);
+        console.log('response', response, response2)
+    }
+
+    // const onError = (errors, e) => console.log(errors, e);
+
+    return (
+        <div className="w-full">
+            <div className="flex items-center justify-center py-12">
+                <div className="mx-auto grid w-[350px] gap-6">
+                    <form onSubmit={handleSubmit(onSubmit)}>
+                        <div className="grid gap-2 text-center">
+                            <h1 className="text-3xl font-bold">Download</h1>
+                            <p className="text-balance text-muted-foreground">
+                                Insira o link do vídeo para download
+                            </p>
+                        </div>
+                        <div className="grid gap-4">
+                            <div className="grid gap-2">
+                                <Label htmlFor="link">Link</Label>
+                                <Input
+                                    id="link"
+                                    type="url"
+                                    placeholder="https..."
+                                    defaultValue={"https://www.youtube.com/watch?v=I_BBfcIcv0g"}
+                                    required
+                                    {...register('url')}
+                                />
+                            </div>
+                            <Button type="submit" className="w-full">
+                                Analisar vídeo
+                            </Button>
+                            {downloadAvailable &&
+                                <Button type="button" className="w-full">
+                                    Analisar vídeo outro
+                                </Button>}
+                        </div>
+                        <div className="mt-4 text-center text-sm">
+                            Está dando algum problema?{" "}
+                            <Link href="#" className="underline">
+                                Tente novamente
+                            </Link>
+                        </div>
+                    </form>
+                </div>
             </div>
-            <div className="grid gap-4">
-              <div className="grid gap-2">
-                <Label htmlFor="link">Link</Label>
-                <Input
-                    id="link"
-                    type="url"
-                    placeholder="https..."
-                    required
-                />
-              </div>
-              <Button type="submit" className="w-full">
-                Analisar vídeo
-              </Button>
-            </div>
-            <div className="mt-4 text-center text-sm">
-              Está dando algum problema?{" "}
-              <Link href="#" className="underline">
-                Tente novamente
-              </Link>
-            </div>
-          </div>
         </div>
-      </div>
-  )
+    )
 }
--- src/app/usecase/GetVideoInfo.ts
+++ src/app/usecase/GetVideoInfo.ts
@@ -1,7 +1,35 @@
+"use client"
+
+import ytdl from 'ytdl-core';
+import { invoke } from '@tauri-apps/api/tauri';
+
 export default class GetVideoInfo {
 
     public execute(videoURL: string): string {
         return 'info';
+    }
+
+    public async getVideoInfo(videoUrl: string) {
+        console.log('videoURL', videoUrl)
+        const info = await invoke('fetch_video_info', { videoUrl });
+        console.log(info);
+        // const {
+        //     player_response: {
+        //         videoDetails: {title, author},
+        //     },
+        // } = await ytdl.getInfo(videoURL);
+        // return {
+        //     title,
+        //     author,
+        // };
+    }
+
+    public async download(videoUrl: string) {
+        console.log('videoURL', videoUrl)
+        const outputPath = '/Users/joaogabriel/env-dev/temp/mp3-downloads';
+        const fileName = 'audio.mp3';
+        const info = await invoke('download_audio_as_mp3', { videoUrl, outputPath, fileNamer });
+        console.log(info);
     }
 
 }
