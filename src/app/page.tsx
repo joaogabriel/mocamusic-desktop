ed6d5913ea37f340554eb410a55c0ab96653b4a7
João Gabriel Esteves <jgs.esteves@gmail.com>
2024-06-18 19:09:35 -0300

    Partial implementation of form validation (still with the old form code)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -17,9 +17,21 @@ import DownloadAudioRequest from "@/app/domain/model/DownloadAudioRequest";
 import DownloadAudio from "@/app/usecase/DownloadAudio";
 import {invokeTauriCommand} from "@tauri-apps/api/helpers/tauri";
 import ClipboardInspect from "@/components/internal/clipboard-inspect";
+import {Form, FormControl, FormDescription, FormField, FormItem, FormLabel, FormMessage} from "@/components/ui/form";
+
+const youtubeRegex = new RegExp('^(https?://)?((www.)?youtube.com/watch\\?v=.+|youtu.be/.+)');
 
 const downloadVideoSchema = z.object({
-    url: z.string().url()
+    // url: z.string().url()
+    url: z.string()
+        .min(3, {
+            message: "URL muito pequena"
+        })
+        .url({
+            message: 'Deve ser uma URL válida'
+        })
+        .regex(youtubeRegex, 'Deve ser uma URL do YouTube'),
+    musicName: z.string()
 });
 
 type DownloadVideoSchema = z.infer<typeof downloadVideoSchema>;
@@ -37,16 +49,30 @@ export default function Page() {
         resolver: zodResolver(downloadVideoSchema),
     });
 
+    const form = useForm<z.infer<typeof downloadVideoSchema>>({
+        resolver: zodResolver(downloadVideoSchema),
+        defaultValues: {
+            // url: 'https://www.google.com/',
+            url: 'https://www.youtube.com/watch?v=jfKfPfyJRdk',
+            musicName: ''
+        },
+    })
+
     const updateVideoUrl = async (url: string) => {
         console.log('updateVideoUrl', url)
         setVideoUrl(url);
         await onSubmit({
-            url: url
+            url: url,
+            musicName: ''
         });
     }
 
     async function onSubmit(data: DownloadVideoSchema) {
         console.log('onSubmit')
+        // TODO exemplo de validacao manual do form
+        form.setError('musicName', {
+            message: 'erro manual'
+        })
         console.log(data)
         setVideoInfoLoading(true);
         const getVideoInfo = new GetVideoInfo();
@@ -129,70 +155,146 @@ export default function Page() {
             <ClipboardInspect updateVideoUrl={updateVideoUrl}></ClipboardInspect>
             <div className="flex items-center justify-center py-12">
                 <div className="mx-auto grid w-[500px] gap-6">
-                    <form onSubmit={handleSubmit(onSubmit)}>
-                        <div className="grid gap-2 text-center">
-                            <h1 className="text-3xl font-bold">Download</h1>
-                            <p className="text-balance text-muted-foreground">
-                                Insira o link do vídeo para download
-                            </p>
-                        </div>
-                        <div className="grid gap-4">
-                            <div className="grid gap-2">
-                                <Label htmlFor="link">Link</Label>
-                                <Input
-                                    id="link"
-                                    type="url"
-                                    placeholder="https..."
-                                    defaultValue={"https://www.youtube.com/watch?v=I_BBfcIcv0g"}
-                                    disabled={downloadAvailable}
-                                    required
-                                    {...register('url')}
+                    {/*<form onSubmit={handleSubmit(onSubmit)}>*/}
+                    {/*    <div className="grid gap-2 text-center">*/}
+                    {/*        <h1 className="text-3xl font-bold">Download</h1>*/}
+                    {/*        <p className="text-balance text-muted-foreground">*/}
+                    {/*            Insira o link do vídeo para download*/}
+                    {/*        </p>*/}
+                    {/*    </div>*/}
+                    {/*    <div className="grid gap-4">*/}
+                    {/*        <div className="grid gap-2">*/}
+                    {/*            <Label htmlFor="link">Link</Label>*/}
+                    {/*            <Input*/}
+                    {/*                id="link"*/}
+                    {/*                type="url"*/}
+                    {/*                placeholder="https..."*/}
+                    {/*                defaultValue={"https://www.google.com/"}*/}
+                    {/*                disabled={downloadAvailable}*/}
+                    {/*                required*/}
+                    {/*                {...register('url')}*/}
+                    {/*            />*/}
+                    {/*        </div>*/}
+                    {/*        {videoInfoLoading &&*/}
+                    {/*            <Button disabled className="flex justify-center items-center">*/}
+                    {/*                <Loader2 className="h-4 animate-spin"/>*/}
+                    {/*                Analisando vídeo...*/}
+                    {/*            </Button>}*/}
+                    {/*        {!videoInfoLoading && !downloadAvailable &&*/}
+                    {/*            <div className="flex justify-center items-center">*/}
+                    {/*                <Button type="submit" className="w-full">*/}
+                    {/*                    Analisar vídeo*/}
+                    {/*                </Button>*/}
+                    {/*            </div>*/}
+                    {/*        }*/}
+                    {/*        {downloadAvailable &&*/}
+                    {/*            <div className="grid gap-2">*/}
+                    {/*                <Label htmlFor="link">Nome da música</Label>*/}
+                    {/*                <Input*/}
+                    {/*                    id="link"*/}
+                    {/*                    type="url"*/}
+                    {/*                    placeholder="Musica.mp3"*/}
+                    {/*                    value={musicName}*/}
+                    {/*                    required*/}
+                    {/*                    onChange={handleMusicName}*/}
+                    {/*                />*/}
+                    {/*            </div>*/}
+                    {/*        }*/}
+                    {/*        {downloadAvailable && downloading &&*/}
+                    {/*            <Button disabled className="flex justify-center items-center">*/}
+                    {/*                <Loader2 className="h-4 w-4 animate-spin"/>*/}
+                    {/*                Baixando música...*/}
+                    {/*            </Button>*/}
+                    {/*        }*/}
+                    {/*        {downloadAvailable && !downloading &&*/}
+                    {/*            <Button type="button" className="w-full" onClick={() => download()}>*/}
+                    {/*                Baixar música*/}
+                    {/*            </Button>*/}
+                    {/*        }*/}
+                    {/*    </div>*/}
+                    {/*    <div className="mt-4 text-center text-sm">*/}
+                    {/*        Quer começar de novo ou baixar outra música?{" "}*/}
+                    {/*        <Link href="#" className="underline" onClick={() => resetFormState()}>*/}
+                    {/*            Clique aqui*/}
+                    {/*        </Link>*/}
+                    {/*    </div>*/}
+                    {/*</form>*/}
+                    <Form {...form}>
+                        <form onSubmit={form.handleSubmit(onSubmit)}>
+                            <div className="grid gap-4">
+                                <FormField
+                                    control={form.control}
+                                    name="url"
+                                    render={({field}) => (
+                                        <FormItem>
+                                            <FormLabel>Link</FormLabel>
+                                            <FormControl>
+                                                <Input
+                                                    id="link"
+                                                    type="url"
+                                                    placeholder="https..."
+                                                    disabled={downloadAvailable}
+                                                    required
+                                                    {...field}
+                                                />
+                                            </FormControl>
+                                            <FormMessage/>
+                                        </FormItem>
+                                    )}
                                 />
-                            </div>
-                            {videoInfoLoading &&
-                                <Button disabled className="flex justify-center items-center">
-                                    <Loader2 className="h-4 animate-spin"/>
-                                    Analisando vídeo...
-                                </Button>}
-                            {!videoInfoLoading && !downloadAvailable &&
-                                <div className="flex justify-center items-center">
-                                    <Button type="submit" className="w-full">
-                                        Analisar vídeo
-                                    </Button>
-                                </div>
-                            }
-                            {downloadAvailable &&
-                                <div className="grid gap-2">
-                                    <Label htmlFor="link">Nome da música</Label>
-                                    <Input
-                                        id="link"
-                                        type="url"
-                                        placeholder="Musica.mp3"
-                                        value={musicName}
-                                        required
-                                        onChange={handleMusicName}
+                                {videoInfoLoading &&
+                                    <Button disabled className="flex justify-center items-center">
+                                        <Loader2 className="h-4 animate-spin"/>
+                                        Analisando vídeo...
+                                    </Button>}
+                                {!videoInfoLoading && !downloadAvailable &&
+                                    <div className="flex justify-center items-center">
+                                        <Button type="submit" className="w-full">
+                                            Analisar vídeo
+                                        </Button>
+                                    </div>
+                                }
+                                {downloadAvailable && (
+                                    <FormField
+                                        control={form.control}
+                                        name="musicName"
+                                        render={({field}) => (
+                                            <FormItem>
+                                                <FormLabel>Nome da música</FormLabel>
+                                                <FormControl>
+                                                    <Input
+                                                        id="link"
+                                                        type="text"
+                                                        placeholder="Musica.mp3"
+                                                        required
+                                                        {...field}
+                                                    />
+                                                </FormControl>
+                                                <FormMessage/>
+                                            </FormItem>
+                                        )}
                                     />
+                                )}
+                                {downloadAvailable && downloading &&
+                                    <Button disabled className="flex justify-center items-center">
+                                        <Loader2 className="h-4 w-4 animate-spin"/>
+                                        Baixando música...
+                                    </Button>
+                                }
+                                {downloadAvailable && !downloading &&
+                                    <Button type="button" className="w-full" onClick={() => download()}>
+                                        Baixar música
+                                    </Button>
+                                }
+                                <div className="mt-4 text-center text-sm">
+                                    Quer começar de novo ou baixar outra música?{" "}
+                                    <Link href="#" className="underline" onClick={() => resetFormState()}>
+                                        Clique aqui
+                                    </Link>
                                 </div>
-                            }
-                            {downloadAvailable && downloading &&
-                                <Button disabled className="flex justify-center items-center">
-                                    <Loader2 className="h-4 w-4 animate-spin"/>
-                                    Baixando música...
-                                </Button>
-                            }
-                            {downloadAvailable && !downloading &&
-                                <Button type="button" className="w-full" onClick={() => download()}>
-                                    Baixar música
-                                </Button>
-                            }
-                        </div>
-                        <div className="mt-4 text-center text-sm">
-                            Quer começar de novo ou baixar outra música?{" "}
-                            <Link href="#" className="underline" onClick={() => resetFormState()}>
-                                Clique aqui
-                            </Link>
-                        </div>
-                    </form>
+                            </div>
+                        </form>
+                    </Form>
                 </div>
             </div>
         </div>
